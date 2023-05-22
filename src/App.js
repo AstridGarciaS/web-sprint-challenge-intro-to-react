77fa42d5d68b262eafc54cd2defd0b7232fe6948
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-05-22 13:58:50 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import "./App.css";
-import axios from "axios";
-import characters from "./components/Character"
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  text-align: left;
+  align-items: center;
+  justify-content: space-around;
+  width: 60%;
+`;
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [StarWarsData, setData] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [ startWars, setStartWars] =
-  useState([]);
-  useEffect(() => {
-    axios.get(" https://swapi.dev/api/people/")
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people/')
       .then(res => {
-        console.log(res.data);
-        setStartWars(res.data);
+        setData(res.data)
       })
-      .catch(err =>
-        console.log(err));
-      }, []);
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
+
+  console.log(StarWarsData);
+
+  const characterComponents  = StarWarsData.map(character => (
+    <Character
+      name={character.name}
+      height={character.height}
+      mass={character.mass}
+      hairColor={character.hair_color}
+      skinColor={character.skin_color}
+    />
+  ));
+  
+  
+
   return (
     <div className="App">
-      <h1 className="Header">REACT WARS</h1>
-      {startWars.map(star => {
-        return <characters name = {startWars.name} />;
-      })}
+      <h1 className="Header">Characters</h1>
+      {characterComponents}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+
+const CharacterContainer = styled.div`
+background-color: white;
+padding: 10px;
+margin-bottom: 10px;
+`;
+
+const CharacterName = styled.h2`
+  color: white;
+`;
+
+function Character(props) {
+    const {name, height, mass, hairColor, skinColor } = props
+
+
+
+return (
+    <div className='character'>
+         <h2>{name}</h2>
+         <h3>{height}</h3>
+         <h3>{mass}</h3>
+         <h3>{hairColor}</h3>
+         <h3>{skinColor}</h3>    
+    </div>
+);
+}
+
+
+export default Character;
