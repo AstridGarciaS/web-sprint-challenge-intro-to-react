e14382b671acdfbb0602ca55e91950ff9b7cce57
AstridGarciaS <119351821+AstridGarciaS@users.noreply.github.com>
2023-05-23 15:22:25 -0400

    finish page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
 
-
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -25,9 +24,19 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      {StarWarsData.map(character => {
-        return <Character name={character.name} />
-      })}
+      <div className='CharacterName'>
+        {StarWarsData.map(character => (
+        <Character 
+        key={character.name} 
+        name={character.name}
+        birth_year={character.birth_year}
+        height={character.height}
+        hair_color={character.hair_color}
+        gender={character.gender}
+        skin_color={character.skin_color}
+        />
+        ))}
+       </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,27 +1,80 @@
-// Write your Character component here
 import styled from "styled-components";
-import React from "react";
-
+import React, { useState } from "react";
 
 const CharacterContainer = styled.div`
-background-color: white;
-padding: 1px;
-margin-bottom: 10px;
-color: white;
+background-color: #292c0b46;
+align-items center;
+font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+font-size: 25px;
+color: #ffffff;
+height: 100px;
+display: flex;
+justify-content: center;
+position: relative;
+
+.name {
+    margin-bottom: 10px;
+  }
+
+  .cardSection {
+    position: absolute; 
+    top: 100%; 
+    left: 0; 
+    display: flex; 
+  }
+
+  .character:hover .cardSection {
+    display: flex; 
+  }
+
+  .card {
+    text-align:center;
+  }
+
+  .card p {
+    margin: 5px;
+  }
+
+  p{
+    font-size: 15px;
+    display: flex;
+    justify-content: left;
+
+  }
 `;
 
 
 const Character = (props) => {
-    const {name} = props
+  const { name, birth_year, height, gender, hair_color, skin_color } = props;
+  const [isHovered, setIsHovered] = useState(false);
 
-    
-return (
-    <div className='character'>
-         <h2>{props.name}</h2>
-    </div>
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-);
-}
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
+  
+  return (
+    <CharacterContainer className="character"
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}>
+      <div className="name">
+        {name} 
+      </div>
+      {isHovered && (
+        <div className="extraInfo">
+          <p>Year: {birth_year}</p>
+          <p>height: {height}</p>
+          <p>Gender: {gender}</p>
+          <p>Hair Color: {hair_color}</p>
+          <p>Skin Color: {skin_color}</p>
+        </div>
+      )}
+    </CharacterContainer>
+  );
+};
 
 export default Character;
